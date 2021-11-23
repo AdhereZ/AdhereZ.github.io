08824f8a14f03fdb9f90e2169225f2a9c5eaec14
Adhere <291645896@qq.com>
2021-11-23 22:11:29 +0800

    init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,37 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '技术',
+    items: [
+      { text: '前端', link: '/frontend/' },
+      { text: '后端', link: '/backend/' },
+    ],
+  },
+  {
+    text: '更多',
+    items: [
+      { text: '其他', link: '/other/' },
+    ],
+  },
+  { text: '友链', link: '/friends/' },
+  { text: '关于', link: '/about/' },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    // items: [
+    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    // ],
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
